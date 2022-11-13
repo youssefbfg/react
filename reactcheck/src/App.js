95d7dce3a3f6a897f5fb7bb2f39188f79a5b7e5d
youssef <bufalgha@gmail.com>
2022-11-13 18:38:35 +0100

    first commit

--- reactcheck/src/App.js
+++ reactcheck/src/App.js
@@ -1,36 +1,15 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css";
+import Profile from "./Profile";
 
-function App() {
+export default function App() {
+  const myFunction = (myName) => {
+    alert(` ${myName} is a student he is lying `);
+  };
 
   return (
-  <div>
-    <h1>hey!</h1>
-    
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div className="App">
+      <Profile Fullname="Youssef Boufalgha" myFunction={myFunction} Bio="owner of seven software engineering companies" Proffession="he is lying"/>
+          < img src="Jeremy_Spector_MD.png"/>
     </div>
   );
 }
-
-
-export default App;
